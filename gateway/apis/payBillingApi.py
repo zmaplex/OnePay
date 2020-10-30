d8f2c29c5616fdf82e1cbb3aec61c77c3687c912
Zmaplex <zmaplex@gmail.com>
2020-10-30 19:38:22 +0800

    add billing query api

---
+++ gateway/apis/payBillingApi.py
@@ -0,0 +1,30 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from gateway.apis.payBillingDoc import base_pay_billing_view__query_order
+from gateway.models import Billing
+from gateway.serializers.payBillingSz import PayBillingSerializer, BasePayQueryBillingSerializer
+
+
+class BasePayBillingView(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (permissions.IsAdminUser,)
+    queryset = Billing.objects.all()
+    serializer_class = PayBillingSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = []
+
+    @swagger_auto_schema(**base_pay_billing_view__query_order)
+    @action(methods=['get'], permission_classes=[permissions.AllowAny],
+            detail=False, serializer_class=BasePayQueryBillingSerializer)
+    def query_order(self, request, *args, **kwargs):
+        """
+        查询订单接口，具体参数请查阅 swagger 接口
+        """
+        data = request.query_params
+        serializer = BasePayQueryBillingSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            result = serializer.save()
+            return Response(result.data)
---
+++ gateway/apis/payBillingDoc.py
@@ -0,0 +1,16 @@
+from drf_yasg import openapi
+
+from gateway.serializers.payBillingSz import PayBillingSerializer
+
+base_pay_billing_view__query_order = {
+    'operation_summary': 'sid 与 pid 必填一个',
+    'manual_parameters': [
+        openapi.Parameter(name='sid', required=False, in_=openapi.IN_QUERY, description='本系统账单id',
+                          type=openapi.TYPE_STRING),
+        openapi.Parameter(name='pid', required=False, in_=openapi.IN_QUERY, description='支付平台账单id',
+                          type=openapi.TYPE_STRING),
+        openapi.Parameter(name='app_id', in_=openapi.IN_QUERY, description='应用id', type=openapi.TYPE_STRING),
+        openapi.Parameter(name='sign', in_=openapi.IN_QUERY, description='签名', type=openapi.TYPE_STRING),
+    ],
+    'responses': {200: openapi.Response('response description', PayBillingSerializer)}
+}
